81bdc7f5b5c95786f01fac2e2628b2843d067aed
Idris Khan <idris.khan303@gmail.com>
2022-10-08 12:49:10 +0100

    Flexbox deepdive

--- section-2-basics/Deep-dive-flexbox/App.js
+++ section-2-basics/Deep-dive-flexbox/App.js
@@ -3,39 +3,45 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    <View
+      style={{
+        padding: 50,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+      }}
+    >
       <View
         style={{
           backgroundColor: 'red',
-          width: 100,
-          height: 100,
           justifyContent: 'center',
-          alignItems: 'center'
+          alignItems: 'center',
+          flex: 1,
         }}
       >
-        <Text>1</Text>
+        <Text style={{ color: 'white' }}>1</Text>
       </View>
       <View
         style={{
           backgroundColor: 'blue',
-          width: 100,
-          height: 100,
           justifyContent: 'center',
-          alignItems: 'center'
+          alignItems: 'center',
+          flex: 2,
         }}
       >
-        <Text>2</Text>
+        <Text style={{ color: 'white' }}>2</Text>
       </View>
       <View
         style={{
           backgroundColor: 'green',
-          width: 100,
-          height: 100,
           justifyContent: 'center',
-          alignItems: 'center'
+          alignItems: 'center',
+          flex: 1,
         }}
       >
-        <Text>3</Text>
+        <Text style={{ color: 'white' }}>3</Text>
       </View>
     </View>
   );
