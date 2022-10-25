2eda7a710fe82905cffd223ea2dcf51a238fa13c
Idris Khan <idris.khan303@gmail.com>
2022-10-25 17:57:39 +0100

    tab navigator

--- section-5-navigation/Drawer-Navigation/App.jsx
+++ section-5-navigation/Drawer-Navigation/App.jsx
@@ -1,42 +1,39 @@
 import 'react-native-gesture-handler';
 import { Platform, SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 import { color } from 'react-native-reanimated';
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      <BottomTab.Navigator
         screenOptions={{
-          drawerActiveBackgroundColor: '#aaa',
-          drawerActiveTintColor: '#fff',
-          headerStyle: { backgroundColor: '#ccc' },
-          headerTintColor: '#000',
-          drawerStyle: { backgroundColor: '#fff' },
+          headerStyle: { backgroundColor: '#3c0a6b' },
+          tabBarActiveTintColor: '#3c0a6b',
+          headerTintColor: '#fff',
         }}
       >
-        <Drawer.Screen
+        <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            drawerLabel: 'Welcome Screen',
-            drawerIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+            tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name="User"
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
+            tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
--- section-5-navigation/Drawer-Navigation/screens/UserScreen.jsx
+++ section-5-navigation/Drawer-Navigation/screens/UserScreen.jsx
@@ -1,11 +1,16 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function UserScreen() {
+export default function UserScreen({ route, navigation }) {
+  function openDrawerHandler() {
+    navigation.toggleDrawer();
+  }
+
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title="Open Drawer" onPress={openDrawerHandler} />
     </View>
   );
 }
