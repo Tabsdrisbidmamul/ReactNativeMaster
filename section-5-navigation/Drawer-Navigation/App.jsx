4a47de4327f217e746e272a44da20f98017d4ce4
Idris Khan <idris.khan303@gmail.com>
2022-10-25 09:15:09 +0100

    drawer navgation

---
+++ section-5-navigation/Drawer-Navigation/App.jsx
@@ -0,0 +1,29 @@
+import 'react-native-gesture-handler';
+import { Platform, SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight + 20,
+      },
+    }),
+  },
+});
