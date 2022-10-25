3efb5512e304c8b82df36d15eb6fb35d78555169
Tabsdrisbidmamul <idris.khan303@gmail.com>
2022-10-25 13:44:32 +0100

    drawer options covered

--- section-5-navigation/Drawer-Navigation/App.jsx
+++ section-5-navigation/Drawer-Navigation/App.jsx
@@ -2,17 +2,40 @@ import 'react-native-gesture-handler';
 import { Platform, SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
+import { color } from 'react-native-reanimated';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          drawerActiveBackgroundColor: '#aaa',
+          drawerActiveTintColor: '#fff',
+          headerStyle: { backgroundColor: '#ccc' },
+          headerTintColor: '#000',
+          drawerStyle: { backgroundColor: '#fff' },
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
